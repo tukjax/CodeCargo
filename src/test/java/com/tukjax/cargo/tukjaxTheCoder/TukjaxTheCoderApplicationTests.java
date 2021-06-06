19cffc3f9d83bf10c527776d1cc788240f51b8b5
tukjax <tukjax@gmail.com>
2021-06-06 19:08:00 +0800

    init commit

---
+++ src/test/java/com/tukjax/cargo/tukjaxTheCoder/TukjaxTheCoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tukjax.cargo.tukjaxTheCoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukjaxTheCoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
